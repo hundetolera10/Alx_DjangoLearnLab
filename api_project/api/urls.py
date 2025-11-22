52814cb49e030fa9c8c830b6efd15432fdff5d47
Hunde Tolera <hundetolera225@gmail>
2025-11-22 16:25:23 +0300

    Add api_project with DRF CRUD and authentication

---
+++ api_project/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from django.urls import include
+from .views import BookList
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
+    path('api/token/', obtain_auth_token, name='api-token'),
+]
---
+++ api_project/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+# Create your views here.

1cb31e94ed675a0e14a0439489ffffb2940d5fc3
Hunde Tolera <hundetolera225@gmail>
2025-12-06 23:29:15 +0300

    modify post creation and update forms

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import Tagwidget
-from .models import Comment, Post, Tag
+from taggit.forms import TagWidget  # Import TagWidget from taggit
+from .models import Comment, Post
+
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
+
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
@@ -42,9 +44,7 @@ class CommentForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "content"]
+        fields = ["title", "content", "tags"]  # Include tags in the form
         widgets = {
-            "title": forms.TextInput(attrs={"placeholder": "Post Title"}),
-            "content": forms.Textarea(attrs={"rows": 5, "placeholder": "Write your post here..."}),
-            'tags': Tagwidget(),
+            "tags": TagWidget(),  # Use TagWidget for tag input
         }

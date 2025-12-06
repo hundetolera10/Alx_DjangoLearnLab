9fb656081bafdee85ed40bce0c690f35c29ce620
Hunde Tolera <hundetolera225@gmail>
2025-12-06 23:32:19 +0300

    implementation of the comment

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -33,7 +33,7 @@ class Comment(models.Model):
     post = models.ForeignKey(post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
